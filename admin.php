1726c21eca8812ddaec46975b94e13cac9a332ea
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-11-29 14:17:36 -0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,13 +1,6 @@
 <?php
-
   require_once('header.php');
-  if (!isset($_SESSION['user'])) {
-    echo '<div class="alert alert-danger" role="alert">You aren\'t logged in.</div>';
-    die('</div></div></div></html>');
-  } else {
-
     echo <<< _ADMIN
-
     <h2>Admin Home</h2>
     <hr />
       <div class="row text-center">
@@ -18,8 +11,5 @@
           <a class="btn btn-lg btn-outline-primary" href="post-message.php">Post Message</a>
         </div>
       </div>
-
-
 _ADMIN;
-
   }
