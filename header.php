de237a52de8679cc7563fcc6b74074f1fee4eafb
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-11-29 16:47:22 -0600

    Update header.php

--- header.php
+++ header.php
@@ -13,6 +13,11 @@
       <link href='css/custom.css' rel='stylesheet'>
     </head>
     <body>
+	<script>document.body.style.backgroundImage = " url(https://o.aolcdn.com/images/dims?quality=85&image_uri=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fresize%3D2000%252C2000%252Cshrink%26image_uri%3Dhttps%253A%252F%252Fs.yimg.com%252Fos%252Fcreatr-uploaded-images%252F2019-09%252F0b3cbdc0-ce26-11e9-a5df-7b4ab946142e%26client%3Da1acac3e1b3290917d92%26signature%3D00fedde1916a2d74cd048ce9149a2a73b9b41212&client=amp-blogside-v2&signature=618a2121066af0a0ae7cb3cb87034ae6940dc475)";
+			document.body.style.backgroundRepeat = "no-repeat";
+			document.body.style.backgroundSize = "cover";
+	</script>
+	
       <nav class='navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top'>
         <div class='container'>
           <a class='navbar-brand' href='index.php'> TTU-4354-satellite-map </a>
