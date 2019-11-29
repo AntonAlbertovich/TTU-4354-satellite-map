c9a6d593aa3d4b779b6516458a4120b712f6b328
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-11-29 14:15:45 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,8 @@
 <?php
   session_start();
   require_once('functions.php');
-
   echo <<<_HEADER
-
   <html lang='en'>
-
     <head>
       <meta charset='utf-8'>
       <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
@@ -15,17 +12,11 @@
       <link href='css/bootstrap.min.css' rel='stylesheet'>
       <link href='css/custom.css' rel='stylesheet'>
     </head>
-
     <body>
       <nav class='navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top'>
         <div class='container'>
           <a class='navbar-brand' href='index.php'> TTU-4354-satellite-map </a>
-
 _HEADER;
-
-
-
-
 echo <<<_HEADER_MORE
           </div>
         </div>
@@ -34,18 +25,12 @@ echo <<<_HEADER_MORE
       <div class='container'>
         <div class="row">
             <div class="col-lg-3 mb-4" id="sidebar">
-
 _HEADER_MORE;
-
       if (checkIfLoggedIn()) {
         $user = $_SESSION['user'];
-        if (checkIfAdmin())
-          echo $admin_sidebar;
-        else
+        
           echo $user_sidebar;
-      } else {
-        echo $guest_sidebar;
+      
       }
-
 echo '</div>';
 echo '<div class="col-lg-9 mb-4" id="main_content">';
