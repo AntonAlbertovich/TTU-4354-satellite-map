e43211b67e9383ee6cf471dcedde082caa4e2b56
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-12-01 15:04:08 -0600

    Update search_Expected_Lifetime_yrs.php

--- search_Expected_Lifetime_yrs.php
+++ search_Expected_Lifetime_yrs.php
@@ -1,47 +1,32 @@
 <?php
-
   require_once('header.php');
-
   if (!isset($_SESSION['user'])) {
-
     echo '<div class="alert alert-danger" role="alert">You aren\'t logged in.</div>';
     die('</div></div></div></html>');
-
   } else {
-
     echo $search_form;
-
+	echo '<h2><i><font color="white">Search Expected Lifetime in Years </font></i></h2>';
     if (isset($_GET['q'])) {
-
       $q = $_GET['q'];
-
       $result = queryMySQL("SELECT * FROM main_table WHERE LOWER(Expected_Lifetime_yrs) LIKE '%$q%'");
       $num = $result->num_rows;
-
-      echo '<h2><i>Search Expected Lifetime in years' . $q . '</i></h2>';
-
+      
       if ($num == 0)
       {
         echo '<div class="alert alert-danger" role="alert">No search results found to be matching ' . $q . '</div>';
       }
       else
       {
-
         for ($j = 0 ; $j < $num ; ++$j)
         {
           $row = $result->fetch_array(MYSQLI_ASSOC);
 	echo '<div class="card mb-4">';
           echo '<div class="card-header "><p>'  .   make_clickable($row['user_id']). '</p></div>';
         echo '<div class="card-body">';
-          echo '<p class="card-text">' . 'Country ' . make_clickable($row['Country_Org_of_UN_Registry']) .  ' -- ' . make_clickable($row['Country_of_Operator_Owner']) . '!' . make_clickable($row['Operator_Owner']) . ' ' . make_clickable($row['Users']) .  ' ' . make_clickable($row['Purpose']) . ' ' . make_clickable($row['Date_of_Launch']) . ' ' . make_clickable($row['Expected_Lifetime_yrs']) .  ' ' . make_clickable($row['Contractor']) . ' ' . make_clickable($row['Country_of_Contractor']) .  ' ' . make_clickable($row['Launch_Site']) . ' ' .  '</p></div></div>';
-
+        echo '<p class="card-text">' . 'Country of Origin: ' . make_clickable($row['Country_Org_of_UN_Registry']) .  ' Country of Operator: ' . make_clickable($row['Country_of_Operator_Owner']) . ' Operator: ' . make_clickable($row['Operator_Owner']) . ' User: ' . make_clickable($row['Users']) .  ' Purpose: ' . make_clickable($row['Purpose']) . ' Launch Date: ' . make_clickable($row['Date_of_Launch']) . ' Life Expectancy: ' . make_clickable($row['Expected_Lifetime_yrs']) .  ' years Contractor: ' . make_clickable($row['Contractor']) . ' Country of Contractor: ' . make_clickable($row['Country_of_Contractor']) .  ' Launch Site: ' . make_clickable($row['Launch_Site']) . ' ' .  '</p></div></div>';
         }
-
         echo '  </tbody>
         </table>';
-
       }
-
     }
-
   }
